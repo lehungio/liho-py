dc283427aa2af6a5d835c4e013f6458345399125
HL <lecaoquochung@gmail.com>
2017-06-14 13:36:49 +0900

    Hello python

---
+++ hello.py
@@ -0,0 +1,21 @@
+print('Hello world!')
+print('Welcome to Python')
+
+# elif
+num=1
+if num == 1:
+    print("Number is 1")
+else:
+    print ("Something")
+
+# not
+a=not(1)
+print(a)
+
+#
+if not True:
+   print("1")
+elif not (1 + 1 == 3):
+   print("2")
+else:
+   print("3")
